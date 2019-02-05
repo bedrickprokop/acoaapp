5ed35f01122740a5f223161f669cc380380acb2e
bedrick <bedrick.prokop@gmail.com>
2019-02-05 16:23:52 -0200

    Test ui creation and gradle configurations

--- app/src/androidTest/java/br/com/acoaapp/main/login/LoginScreenTest.java
+++ app/src/androidTest/java/br/com/acoaapp/main/login/LoginScreenTest.java
@@ -1,5 +1,7 @@
 package br.com.acoaapp.main.login;
 
+import android.support.test.BuildConfig;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -10,24 +12,39 @@ import org.junit.runner.RunWith;
 
 import br.com.acoaapp.R;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
+@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class LoginScreenTest {
 
-    LoginActivity loginActivity;
+    private LoginActivity loginActivity;
+    private String email;
+    private String password;
 
+    //TODO set other values according to BuildConfig.FLAVOR - mock, debug, etc
     @Rule
     public ActivityTestRule<LoginActivity> loginActivityTestRule =
             new ActivityTestRule<>(LoginActivity.class, false, true);
 
 
     @Before
-    public void setup(){
+    public void setup() {
         loginActivity = loginActivityTestRule.getActivity();
+
+        if (BuildConfig.FLAVOR.equals("mock")) {
+            email = "bed.rick@gmail.com";
+            password = "password";
+        } else {
+            email = "bed.rick@yahoo.com";
+            password = "password";
+        }
     }
 
     @Test
@@ -37,5 +54,15 @@ public class LoginScreenTest {
         onView(withId(R.id.et_email)).check(matches(isDisplayed()));
         onView(withId(R.id.et_password)).check(matches(isDisplayed()));
         onView(withId(R.id.bt_login)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void whenFillAllFields_andPressEnterButton_shouldOpenInitialScreen() {
+        onView(withId(R.id.et_email)).perform(typeText(email));
+        closeSoftKeyboard();
+        onView(withId(R.id.et_password)).perform(typeText(password));
+        closeSoftKeyboard();
+        onView(withId(R.id.bt_login)).perform(click());
+        //onView(withText("Estatísticas gerais")).check(matches(isDisplayed()));
     }
 }
